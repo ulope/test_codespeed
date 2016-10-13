4aaa9c1d4a6f8b32835a4566cfc8e54942e2a128
Ulrich Petri <ulo@ulo.pe>
2016-10-13 17:05:16 +0200

    slower Thu Oct 13 17:05:16 CEST 2016

--- test.py
+++ test.py
@@ -6,4 +6,9 @@ import pytest
 
 @pytest.mark.timeit(n=50000, r=5, mode='fast')
 def test_hash():
-    hashlib.sha256(b"abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz")
+    hashlib.sha256(
+        b"abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz"
+        b"abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz"
+        b"abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz"
+        b"abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz"
+    )
