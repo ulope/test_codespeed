5762e7496d8e8797143efe8358398d65afe853ea
Ulrich Petri <ulo@ulo.pe>
2016-10-13 15:22:33 +0200

    Thu Oct 13 15:22:33 CEST 2016

--- test.py
+++ test.py
@@ -3,18 +3,13 @@ import time
 
 import pytest
 
-@pytest.mark.timeit(n=1000, r=5, mode='fast')
+@pytest.mark.timeit(n=100000, r=5, mode='fast')
 def test_simple():
     s = ""
-    for i in range(1000):
+    for i in range(100):
         s = s + "a"
 
 
-@pytest.mark.timeit(n=100000, r=15, mode='fast')
+@pytest.mark.timeit(n=1000000, r=15, mode='fast')
 def test_hash():
     hashlib.sha256(b"abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz")
-
-
-@pytest.mark.timeit(n=1000, r=3)
-def test_hash_safe():
-    hashlib.sha256(b"abcdefghijklmnopqrstuvwxyz0")
